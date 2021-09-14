58cbe1ba9bf851bebfbc42627a6b2694d7bd8c3e
Frepap3 <62154673+Frepap3@users.noreply.github.com>
2021-09-14 17:24:40 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'YOUR NAME',                   # Type in your name
   author_email = 'diablo@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/Frepap3/Frepap_Alternativa2',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Frepap3/Frepap_Alternativa/archive/refs/tags/Frepap_Alternativa.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Frepap3/Frepap_Alternativa2/archive/refs/tags/Frepap_Alternativa2.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'random2',
